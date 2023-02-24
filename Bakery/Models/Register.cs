5767cc05f488f3cc1d97ca1ca041d85a878b5a1a
Kai Clausen <kaiclausen123@gmail.com>
2023-02-24 13:18:12 -0800

    refactor with inheritance

--- Bakery/Models/Register.cs
+++ Bakery/Models/Register.cs
@@ -4,8 +4,8 @@ namespace Bakery.Models
 {
   public class Register
   {
-    public int Price { get; }
-    public int Discount { get; }
+    public int Price { get; set; }
+    public int Discount { get; set; }
 
     public Register()
     {
@@ -21,10 +21,8 @@ namespace Bakery.Models
     }
   }
   
-  public class Bread 
+  public class Bread : Register
   {
-    public int Price { get; }
-    public int Discount { get; }
     public Bread()
     {
       Price = 5;
@@ -32,10 +30,8 @@ namespace Bakery.Models
     }
   }
   
-  public class Pastry
+  public class Pastry : Register
   {
-    public int Price { get; }
-    public int Discount { get; }
     public Pastry()
     {
       Price = 2;
@@ -43,36 +39,3 @@ namespace Bakery.Models
     }
   } 
 }
-
-// namespace Bakery.Models
-// {
-//   public class Register
-//   {
-//     public int Price { get; set; }
-//     public int Discount { get; set; }
-//     public int GetPrice(int input)
-//     {
-//       int remainder = input/this.Discount;
-//       int price = this.Price;
-//       int total = price * (input-remainder); 
-//       return total;
-//     }
-//   }
-//   public class Bread : Register
-//   {
-//     public Bread()
-//     {
-//       Discount = 3;
-//       Price = 5;
-//     }
-//   }
-//   public class Pastry : Register
-//   {
-//     public Pastry()
-//     {
-//       Discount = 4;
-//       Price = 2;
-//     }
-//   }
-  
-// }
