cef051685912c03b22c1c07261e1aa1e3c220664
Kai Clausen <kaiclausen123@gmail.com>
2023-02-24 11:07:20 -0800

    created Bread class and constructor

--- Bakery/Models/Register.cs
+++ Bakery/Models/Register.cs
@@ -1,1 +1,15 @@
 using System;
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    public int Price { get; set; }
+    public int Discount { get; set; }
+    public Bread()
+    {
+      Price = 5;
+      Discount = 3;
+    }
+  }
+}
