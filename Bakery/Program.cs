e2caabf65d22d2895c4b7f39ac4e8a1eaa6b3823
Kai Clausen <kaiclausen123@gmail.com>
2023-02-24 16:49:38 -0800

    handle errors

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,24 +10,24 @@ class Program
       Console.WriteLine("Hello");
       Bread newBread = new Bread();
       Pastry newPastry = new Pastry();
-      Console.WriteLine($"The Price of Bread is {newBread.Price}");
-      Console.WriteLine($"The Price of Pastries is {newPastry.Price}");
+      Console.WriteLine($"The Price of Bread is ${newBread.Price}");
+      Console.WriteLine($"The Price of Pastries is ${newPastry.Price}");
       Console.WriteLine("How many loaves of bread would you like?");
       // int loaves = Int32.Parse(Console.ReadLine());
       string loavesString = Console.ReadLine();
       string[] loavesArr = loavesString.Split(" ");
-      if (loavesArr.Length > 1) 
-      {
-        throw new ArgumentException("Input needs to have no spaces");
-      }
-      int loaves = Int32.Parse(loavesString);
       Console.WriteLine("How many pastries would you like?");
       string pastriesString = Console.ReadLine();
-      string[] pastriesArr = pastriesString.Split("");
-      if (pastriesArr.Length > 1)
+      string[] pastriesArr = pastriesString.Split(" ");
+      if (loavesArr.Length > 1)
       {
-        throw new ArgumentException("Input needs to have no spaces");
+        throw new ArgumentException("Number of Loaves needs to have no spaces");
       }
+      else if (pastriesArr.Length > 1)
+      {
+        throw new ArgumentException("Number of Pastries needs to have no spaces");
+      }
+      int loaves = Int32.Parse(loavesString);
       int pastries = Int32.Parse(pastriesString);
       int breadPrice = newBread.GetTotal(loaves);
       int pastryPrice = newPastry.GetTotal(pastries);
@@ -42,7 +42,7 @@ class Program
     {
       if (ex.Message == "Input string was not in a correct format.")
       {
-        Console.WriteLine("The input needs to be a number");
+        Console.WriteLine("Both inputs need to be a number");
       }
       else
       {
